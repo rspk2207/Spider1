0605e558829548a9e2773fb633d456704d12dabc
rspk2207 <83638002+rspk2207@users.noreply.github.com>
2021-06-09 00:30:48 +0530

    Add files via upload

---
+++ Sscript.js
@@ -0,0 +1,21 @@
+let ftime = document.getElementById("clock");
+function time()
+{
+    let time = new Date();
+    let sec = time.getSeconds();
+    let min = time.getMinutes();
+    let hour = time.getHours();
+    sec = mod(sec);
+    min = mod(min);
+    hour = mod(hour); 
+    ftime.innerHTML = hour + ":" + min + ":" +sec;
+}    
+function mod(numb)
+{
+    if(numb<10)
+    {
+        numb = "0" + numb;
+    }
+    return numb;
+}
+setInterval(time,1000);
