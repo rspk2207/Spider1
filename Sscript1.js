4f9238f69255cde1eebea8486b074c473db108f8
rspk2207 <83638002+rspk2207@users.noreply.github.com>
2021-06-24 17:52:36 +0530

    Add files via upload

--- Sscript1.js
+++ Sscript1.js
@@ -1,14 +1,55 @@
 let subs = document.getElementById("sub");
 let tb = document.getElementById("tabody");
-let i = 1;
+let i = 0;
 subs.addEventListener('click',function(){intake()});
 function intake(){
+i++;
 let events = document.querySelector("#events").value;
 let dates = document.querySelector("#dates").value;
-tb.innerHTML += "<tr><td>" + events + "</td><td>" + dates + "</td><td><button class='delete' id='" + i + "' onclick='delremove(" + i + ")'>DELETE</button></td></tr>";
-i++;
+window.localStorage.setItem("event" + i, events);
+window.localStorage.setItem("date" +i, dates);
+let fevents = window.localStorage.getItem("event"+i);
+let fdates = window.localStorage.getItem("date"+i);
+tb.innerHTML += "<tr><td>" + fevents + "</td><td>" + fdates + "</td><td><button class='delete' id='" + i + "' onclick='delremove(" + i + ")'>DELETE</button></td></tr>";
+let cc = document.querySelectorAll("tr").length;
+window.localStorage.setItem("count",(cc-1));
 }
 function delremove(c){
+if(c == parseInt(window.localStorage.getItem("count"))){
 let elem = document.getElementById(c);
 elem.closest("tr").remove();
+window.localStorage.removeItem("event" + c);
+window.localStorage.removeItem("date"+c);
+let cc = document.getElementsByTagName("tr").length;
+window.localStorage.setItem("count",(cc-1));
+i--;
 }
+else{
+    let elem = document.getElementById(c);
+    elem.closest("tr").remove();
+    window.localStorage.removeItem("event" + c);
+    window.localStorage.removeItem("date"+c);
+    let cc = window.localStorage.getItem("count");
+    for(i=c;i<cc;i++){
+        let bevent = window.localStorage.getItem("event"+ (i+1));
+        let bdate = window.localStorage.getItem("date"+ (i+1));
+        window.localStorage.setItem("event"+ i, bevent);
+        window.localStorage.setItem("date"+ i, bdate);
+    }
+    window.localStorage.setItem("count",(cc-1));
+}
+}
+function addele(){
+    if(localStorage.getItem("count"))
+    {
+    for(let j=1;j<=(window.localStorage.getItem("count"));j++)
+    {
+        let fevents = window.localStorage.getItem("event"+j);
+        let fdates = window.localStorage.getItem("date"+j);
+        tb.innerHTML += "<tr><td>" + fevents + "</td><td>" + fdates + "</td><td><button class='delete' id='" + j + "' onclick='delremove(" + j + ")'>DELETE</button></td></tr>";       
+        i = j;
+    }
+}
+}
+
+window.onload = addele();
